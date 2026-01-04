718f6795d8211dc73a767c813a3e6b0b1d3e9b90
mc4rvalho <mcarvalho093@gmail.com>
2026-01-04 08:09:08 -0300

    Atualizando página Task Page

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronLeftIcon } from "lucide-react";
+import { ChevronLeftIcon } from "lucide-react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Title } from "../components/title";
 
@@ -10,7 +10,7 @@ export function TaskPage() {
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
       <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-        <div className="w-[500px] space-y-4">
+        <div className="w-[500px] mx-auto space-y-4">
           <div className="flex justify-center relative mb-6">
             <button
               onClick={() => navigate(-1)}
